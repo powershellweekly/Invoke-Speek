511b12eaaeeb7758b809846d55a9d730557dcf26
Michael J. Thomas <Michael.J.Thomas@outlook.com>
2021-03-10 13:24:17 -0800

    Add files via upload
    Invoke-Speak GUI Project with PowerShell Studio

---
+++ Invoke-Speak/Globals.ps1
@@ -0,0 +1,19 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+<#
+Program Name: Invoke-Speak
+Author: Michael J. Thomas
+Company: Thomas IT Services
+Contact: Mike@ThomasITServices.com
+Created:  06/21/2019
+Modified: 06/21/2019
+#>
+
+#Add Global Variable and Assembly for Speech to be User for the Project
+Add-Type -AssemblyName System.Speech
+$Global:Speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+
+
